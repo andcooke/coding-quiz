94424610d5cf8ecb9c9e4e2c716168879f2e7e2d
A <andcooke@duck.com>
2022-06-30 20:38:06 -0500

    quiz working, high score submissions are still broken, and still needs CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,3 @@
-//code quiz js
-
-// Landing page with start button
-// When start button is clicked remove welcome msg and append question 1
-// 	add event listener to button
-// 		activity12
-// Start timer
-// User answers question - read chosen element, data value, compare to answer key
-// If correct, next question
-// If incorrect, - 10 seconds from timer, and then next question
-
-// End of quiz
-// Add up user score
-// Display input for user to type their intials
-// Second html and display user name and score - read object from local storage
-
-
-
-/*
-gives the element in html a variable name for Javascript
-let title = document.querySelector(".play-game");
-
-creates new element
-let addedInfo = document.createElement("h2");
-
-gives element text
-addedInfo.textContent = "Click the button below to play the game!";
-  
-places element on the html
-title.appendChild(addedInfo);
-  
-gives classes to element
-addedInfo.classList.add("center", "color");
-*/
-
-
 let questionContent = [
   {
     title: "Commonly used data types DO NOT include:",
@@ -69,71 +33,122 @@ let questionContent = [
   }
 ];
 
-let topScoresEl = document.querySelector(".top-scores")
-let rootEl = document.querySelector("main");
-let titleEl = document.querySelector(".play-game");
+let rootEl = document.querySelector(".root")
 let startGameEl = document.querySelector("#start-game-btn");
-let questionSpaceEl = document.querySelector(".question");
+let titleEl = document.querySelector(".play-game");
+let timerEl = document.querySelector(".timer");
+let questionEl = document.querySelector(".question");
+let topScoresEl = document.querySelector(".top-scores")
+
+let questionNumber = 0;
+let timer = 30;
+let score = 0;
+let timeInterval;
 
 
-
-let counter = 0;
-
-
-
+//to start game
 startGameEl.addEventListener("click", function () {
   titleEl.remove();
-  return startQuestions();
-})
+  timeInterval = setInterval(countdown, 1000);
+  timerEl.textContent = timer;
+  topScoresEl.textContent = "Score: " + score;
+  showQuestion();
+});
 
-let startQuestions = function () {
-  
-  let addedQuestion = document.createElement("h2");
-  let currentQuestion = questionContent[counter].title
-  addedQuestion.textContent = currentQuestion;
-  rootEl.appendChild(addedQuestion)
-  for (let i = 0; i < questionContent[counter].choices.length; i++) {
-    let answerChoice = document.createElement("button");
-    answerChoice.classList.add("ans-btn");
-    answerChoice.textContent = questionContent[counter].choices[i]
-    addedQuestion.appendChild(answerChoice);
-    if (answerChoice.textContent === questionContent[counter].answer) {
-      answerChoice.classList.add("correct-answer");
-    } else {
-      answerChoice.classList.add("wrong-answer");
-    }
+
+
+
+let printScore = function () {
+  let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  highScores.sort(function (a,b) {
+    return b.score - a.score;
+  }) 
+
+}
+
+let countdown = function () {
+  timer--;
+  timerEl.textContent = timer;
+  if (timer <= 0) {
+    endQuiz();
   }
-  //score function 
-  ".ans-btn".addEventListener("click", calculateScore());//score function
+}
 
-  } // else {
-    // return topScores()
-  // }
+let endQuiz = function () {
+  timerEl.textContent = "Time's up!";
+  clearInterval(timeInterval);
+  questionEl.remove();
+  endGame();
+};
 
 
+let endGame = function () {
+  //if score is in top 5
+  printScore();
+  let announcement = document.createElement("h2");
+  announcement.textContent = "Congratulations! New High Score!"
+  rootEl.appendChild(announcement);
+  let initialEl = document.createElement("form");
+  rootEl.appendChild(initialEl)
+  let initialSubmission = document.createElement("input");
+  initialSubmission.setAttribute("type", "text");
+ 
+  initialSubmission.classList.add("initialsSub")
 
-let calculateScore = function () {
-  if (element.classList.contains("correct-answer")) {
-    console.log("Hell Yeah")
+  initialEl.appendChild(initialSubmission);
+  let submitBtn = document.createElement("button");
+  submitBtn.classList.add("submit-btn")
+  submitBtn.textContent = "SUBMIT"
+  rootEl.appendChild(submitBtn)
+  submitBtn.onclick = saveScore;
+}
+
+//onclick function on button that points to new function saveScore.
+let saveScore = function () {
+  let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+  let initials = initialsSub.value.trim()
+  let newScore = {
+    score: score,
+    initials: initials
   }
-
-  // counter++;
-  // if (counter < questionContent.length) {
-  //   return startQuestions()
+  highScores.push(newScore);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+  printScore();
 }
 
 
-//score function
+let showQuestion = function () {
+  questionEl.innerHTML = "";
+  let addedQuestion = document.createElement("h2");
+  let currentQuestion = questionContent[questionNumber].title;
+  addedQuestion.textContent = currentQuestion;
+  questionEl.appendChild(addedQuestion)
+  for (let i = 0; i < questionContent[questionNumber].choices.length; i++) {
+    let answerChoice = document.createElement("button");
+    answerChoice.classList.add("ans-btn");
+    answerChoice.textContent = questionContent[questionNumber].choices[i];
+    answerChoice.setAttribute("value", questionContent[questionNumber].choices[i]);
+    answerChoice.onclick = checkAnswers;
+    questionEl.appendChild(answerChoice);
+  }
+};
 
-//top scores function with play again button.
-
-
-//try again button needs to refresh page
-//window.location.reload();
-
-
-
-//answering a question replaces the text with the next question in the array
-
-
-
+let checkAnswers = function () {
+  if (this.value !== questionContent[questionNumber].answer) {
+    // console.log("wrong test");
+    timer -= 10;
+    if (timer < 0) {
+      endQuiz();
+    }
+  } else {
+    score += 10;
+    topScoresEl.textContent = "Score: " + score;
+    // console.log("correct test")
+    };
+  questionNumber++;
+  if (questionNumber === questionContent.length) {
+    endQuiz();
+  } else {
+    showQuestion();
+  }
+};
